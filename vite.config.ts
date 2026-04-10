7cff180239f3e2f5260c2a9ed0500611d32d075c
nahyun27 <ksknh7@naver.com>
2026-04-10 14:48:47 +0900

    fix: 404 error

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  appType: 'spa',  // 모든 경로를 index.html로 fallback → SPA 새로고침 404 방지
 })
